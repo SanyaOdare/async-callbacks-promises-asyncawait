e19a1228a161a9f4f2c5706ceeefab0ce60d0d63
sanyaodare <sanyaodare@gmail.com>
2020-09-03 14:04:19 +0100

    add promises

--- callbacks.js
+++ callbacks.js
@@ -1,15 +1,3 @@
-const posts = [
-    { title:  'Post One', body: 'This is post one' },
-    { title:  'Post One', body: 'This is post one' },
-];
 
-function getPosts() {
-    setTimeout(() => {
-        let output = '';
-        posts.forEach((post, index) => {
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    }, 1000);
-}
-getPosts();
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
