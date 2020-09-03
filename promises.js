88d72b7b598fcd2fe8ac4199d4c1a555ffec0bd5
sanyaodare <sanyaodare@gmail.com>
2020-09-03 20:30:45 +0100

    add async await

--- promises.js
+++ promises.js
@@ -18,7 +18,7 @@ function createPost(post) {
         setTimeout(() => {
             posts.push(post);
 
-            const error = true;
+            const error = false;
 
             if(!error) {
                 resolve();
@@ -29,6 +29,34 @@ function createPost(post) {
     });    
 }
 
-createPost({ title: 'Post Three', body: 'This is post three' })
-.then(getPosts)
-.catch(err => console.log(err));
+// createPost({ title: 'Post Three', body: 'This is post three' })
+// .then(getPosts)
+// .catch(err => console.log(err));
+
+// ASYNC / AWAIT
+// async function init() {
+//     await createPost({ title: 'Post Three', body: 'This is post three' });
+    
+//     getPosts();
+// }
+
+// init();
+
+// ASYNC / AWAIT / FETCH
+async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
+    console.log(data);
+}
+
+fetchUsers();
+
+// PROMISE.ALL
+// const promise1 = Promise.resolve('Hello World');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
